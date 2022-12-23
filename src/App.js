c38c275ec8859a434299ca8d443affb1fdd92ef2
petitmj <109069260+petitmj@users.noreply.github.com>
2022-12-23 13:06:28 +0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +46,7 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+
+
